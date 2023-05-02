e517a0be755afb326c00e5b845718d5b93a10ca8
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-05-02 15:28:07 +0900

    [#1] MCU-CLIENT 파트 스켈레톤 구현

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import "../app.css"
+</script>
+
+<svelte:head>
+  <title>MCU-CLIENT</title>
+</svelte:head>
+
+<slot/>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Layout from "$lib/components/Layout.svelte";
+</script>
+
+<Layout>
+
+
+</Layout>
