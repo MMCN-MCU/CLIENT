45ca3d983fe16384468a4eec8fc9016b8bc66a4d
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-05-02 16:50:29 +0900

    [#3] socket 연결 완료

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,5 +1,9 @@
 <script>
   import Layout from "$lib/components/Layout.svelte";
+  import { onMount } from "svelte";
+  import { io } from "socket.io-client";
+
+  onMount(() => {});
 </script>
 
 <Layout />
